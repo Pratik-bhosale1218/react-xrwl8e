5e5d2cddd6b0209faf5572fbffc05ad349de4922
Pratik-bhosale1218 <pratikb947@gmail.com>
2021-03-02 13:44:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xrwl8e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xrwl8e)
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+const a = "pratik";
+const element = <h2> Hello ,(pratik)</h2>;
+ReactDOM.render(element, document.getElementById("root"));
